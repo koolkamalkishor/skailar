1e4a3845cc3cdd6d3310f5effd5e179821973e4e
Rzayy <no-reply@skailar.net>
2023-06-11 12:50:10 +0200

    Make the responsive better

--- components/ButtonsAuth/authButtons.tsx
+++ components/ButtonsAuth/authButtons.tsx
@@ -2,6 +2,8 @@
 
 import { signIn, useSession } from 'next-auth/react';
 
+import { useState } from 'react';
+
 import Image from 'next/image';
 
 import discordLogo from '@/public/discord.png';
@@ -10,15 +12,17 @@ import googleLogo from '@/public/google.png';
 import twitchLogo from '@/public/twitch.png';
 
 export function GoogleSignInButton() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
+    setLoading(true);
     signIn('google');
   };
 
-  if (status === 'loading') {
-    return <p>Loading...</p>;
-  }
+  const handlePopupClose = () => {
+    setLoading(false);
+  };
 
   if (session) {
     return (
@@ -32,130 +36,227 @@ export function GoogleSignInButton() {
     <>
       <button
         onClick={handleClick}
-        className="hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
+        className={`hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200 ${loading ? 'loading' : ''}`}
+        disabled={loading}
       >
-        <Image
-          src={googleLogo}
-          alt="Google Logo"
-          width={20}
-          height={20}
-          className="hidden sm:inline-block"
-        />
-        <span className="hidden sm:inline-block ml-4">
-          Continue with Google
-        </span>
+        {loading ? (
+          <div className="loading-text">Loading...</div>
+        ) : (
+          <>
+            <Image
+              src={googleLogo}
+              alt="Google Logo"
+              width={20}
+              height={20}
+              className="hidden sm:inline-block"
+            />
+            <span className="hidden sm:inline-block ml-4">
+              Continue with Google
+            </span>
+          </>
+        )}
       </button>
       <div className="m-5 w-96">
-       <center>
-       <button onClick={handleClick} className="flex sm:hidden">
-        <Image src={googleLogo} alt="Google Logo" width={50} height={50} />
-      </button>
-       </center>
+        <center>
+          <button onClick={handleClick} className="flex sm:hidden">
+            <Image
+              src={googleLogo}
+              className="rounded-lg p-2 bg-white w-14 h-14"
+              alt="Google Logo"
+            />
+          </button>
+        </center>
       </div>
+      <div data-callback="handlePopupClose"></div>
     </>
   );
 }
 
 export function GithubSignInButton() {
-  const handleClick = () => {
-    signIn('github');
-  };
-
-  return (
-    <>
-      <button
-        onClick={handleClick}
-        className="hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
-      >
-        <Image
-          src={githubLogo}
-          alt="Github Logo"
-          width={20}
-          height={20}
-          className="hidden sm:inline-block"
-        />
-        <span className="hidden sm:inline-block ml-4">
-          Continue with Github
-        </span>
-      </button>
-      <div className="m-5 w-96">
-       <center>
-       <button onClick={handleClick} className="flex sm:hidden">
-        <Image src={githubLogo} alt="Github Logo" width={50} height={50} />
-      </button>
-       </center>
-      </div>
-    </>
-  );
+    const { data: session } = useSession();
+    const [loading, setLoading] = useState(false);
+  
+    const handleClick = () => {
+      setLoading(true);
+      signIn('github');
+    };
+  
+    const handlePopupClose = () => {
+      setLoading(false);
+    };
+  
+    if (session) {
+      return (
+        <p className="text-semibold text-white m-1 pt-5">
+          You are already signed in.
+        </p>
+      );
+    }
+  
+    return (
+      <>
+        <button
+          onClick={handleClick}
+          className={`hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200 ${loading ? 'loading' : ''}`}
+          disabled={loading}
+        >
+          {loading ? (
+            <div className="loading-text">Loading...</div>
+          ) : (
+            <>
+              <Image
+                src={githubLogo}
+                alt="Google Logo"
+                width={20}
+                height={20}
+                className="hidden sm:inline-block"
+              />
+              <span className="hidden sm:inline-block ml-4">
+                Continue with Github
+              </span>
+            </>
+          )}
+        </button>
+        <div className="m-5 w-96">
+          <center>
+            <button onClick={handleClick} className="flex sm:hidden">
+              <Image
+                src={githubLogo}
+                className="rounded-lg p-2 bg-white w-14 h-14"
+                alt="Github Logo"
+              />
+            </button>
+          </center>
+        </div>
+        <div data-callback="handlePopupClose"></div>
+      </>
+    );
 }
 
 export function TwitchSignInButton() {
-  const handleClick = () => {
-    signIn('twitch');
-  };
-
-  return (
-    <>
-      <button
-        onClick={handleClick}
-        className="hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
-      >
-        <Image
-          src={twitchLogo}
-          alt="Twitch Logo"
-          width={30}
-          height={30}
-          className="hidden sm:inline-block"
-        />
-        <span className="hidden sm:inline-block ml-4">
-          Continue with Twitch
-        </span>
-      </button>
-      <div className="m-5 w-96">
-       <center>
-       <button onClick={handleClick} className="flex sm:hidden">
-        <Image src={twitchLogo} alt="Twitch Logo" width={70} height={70} />
-      </button>
-       </center>
-      </div>
-    </>
-  );
+    const { data: session } = useSession();
+    const [loading, setLoading] = useState(false);
+  
+    const handleClick = () => {
+      setLoading(true);
+      signIn('twitch');
+    };
+  
+    const handlePopupClose = () => {
+      setLoading(false);
+    };
+  
+    if (session) {
+      return (
+        <p className="text-semibold text-white m-1 pt-5">
+          You are already signed in.
+        </p>
+      );
+    }
+  
+    return (
+      <>
+        <button
+          onClick={handleClick}
+          className={`hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200 ${loading ? 'loading' : ''}`}
+          disabled={loading}
+        >
+          {loading ? (
+            <div className="loading-text">Loading...</div>
+          ) : (
+            <>
+              <Image
+                src={twitchLogo}
+                alt="Twitch Logo"
+                width={30}
+                height={30}
+                className="hidden sm:inline-block"
+              />
+              <span className="hidden sm:inline-block ml-4">
+                Continue with Twitch
+              </span>
+            </>
+          )}
+        </button>
+        <div className="m-5 w-96">
+          <center>
+            <button onClick={handleClick} className="flex sm:hidden">
+              <Image
+                src={twitchLogo}
+                className="rounded-lg p-2 bg-white w-14"
+                alt="Twitch Logo"
+              />
+            </button>
+          </center>
+        </div>
+        <div data-callback="handlePopupClose"></div>
+      </>
+    );
 }
 
 export function DiscordSignInButton() {
-  const handleClick = () => {
-    signIn('discord');
-  };
-  return (
-    <>
-      <button
-        onClick={handleClick}
-        className="hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
-      >
-        <Image
-          src={discordLogo}
-          alt="Discord Logo"
-          width={20}
-          height={20}
-          className="hidden sm:ml-2 sm:inline-block"
-        />
-        <span className="hidden sm:inline-block ml-4">
-          Continue with Discord
-        </span>
-      </button>
-      <div className="m-5 w-96">
-       <center>
-       <button onClick={handleClick} className="flex sm:hidden">
-        <Image src={discordLogo} alt="Discord Logo" width={50} height={50} />
-      </button>
-       </center>
-      </div>
-    </>
-  );
+    const { data: session } = useSession();
+    const [loading, setLoading] = useState(false);
+  
+    const handleClick = () => {
+      setLoading(true);
+      signIn('discord');
+    };
+  
+    const handlePopupClose = () => {
+      setLoading(false);
+    };
+  
+    if (session) {
+      return (
+        <p className="text-semibold text-white m-1 pt-5">
+          You are already signed in.
+        </p>
+      );
+    }
+  
+    return (
+      <>
+        <button
+          onClick={handleClick}
+          className={`hidden w-full sm:flex items-center font-semibold justify-center h-14 px-6 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200 ${loading ? 'loading' : ''}`}
+          disabled={loading}
+        >
+          {loading ? (
+            <div className="loading-text">Loading...</div>
+          ) : (
+            <>
+              <Image
+                src={discordLogo}
+                alt="Google Logo"
+                width={20}
+                height={20}
+                className="hidden sm:inline-block ml-2"
+              />
+              <span className="hidden sm:inline-block ml-4">
+                Continue with Discord
+              </span>
+            </>
+          )}
+        </button>
+        <div className="m-5 w-96">
+          <center>
+            <button onClick={handleClick} className="flex sm:hidden">
+              <Image
+                src={discordLogo}
+                className="rounded-lg p-2 bg-white w-14"
+                alt="Discord Logo"
+              />
+            </button>
+          </center>
+        </div>
+        <div data-callback="handlePopupClose"></div>
+      </>
+    );
 }
 
 export function CredentialsSignInButton() {
   const handleClick = () => {
-    signIn();
+    signIn('credentials');
   };
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -30,7 +30,7 @@ export default function SignInPage() {
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
       <div className="flex flex-col items-center w-1/3 mt-10 p-10 shadow-md">
-        <h1 className="mt-10 mb-4 text-white text-4xl font-bold">Sign In</h1>
+        <h1 className="mt-10 mb-4 text-white text-4xl font-bold">Login</h1>
         <GoogleSignInButton />
         <GithubSignInButton />
         <DiscordSignInButton />
